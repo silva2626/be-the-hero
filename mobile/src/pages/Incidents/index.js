47ad96cfc30e686afef7d6c78b6caef5304c00cc
Allison Silva <allisonsilva1999@gmail.com>
2020-03-29 13:18:46 -0300

    Mobile da Aplicação

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -62,7 +62,7 @@ export default function Incidents(){
             style={styles.incidentsList}
             data={incidents}
             keyExtractor={incident => String(incident.id)}
-            //showsVerticalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
             onEndReached={loadIncidents}
             onEndReachedThreshold={0.2}
             renderItem={({ item : incident })=>(
